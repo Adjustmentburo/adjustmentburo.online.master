747ca3ec1ef07304eb48b2a53d6a7d6b138e0fe6
Adjusmentburo <mobader@yahoo.com>
2020-08-28 18:41:08 +0200

    Initial commit

---
+++ adjustmentburo.online-master/gatsby-config.js
@@ -0,0 +1,49 @@
+module.exports = {
+  siteMetadata: {
+    title: `adjustmentburo.online`,
+    description: `Sociaal Juridisch Adviseur tegen armoede leeft zelf onder de armoedegrens HSP Hoogbegaafd Fibromyalgie Moeder Oplossingsgericht Geeft niet op.`,
+    author: `@moniquebader1`,
+  },
+  pathPrefix: `/adjustmentburo.online`,
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-catch-links`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [] // just in case those previously mentioned remark plugins sound cool :)
+      }
+    }
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
---
+++ adjustmentburo.online-master/src/pages/24-08-2020-getting-started/index.md
@@ -0,0 +1,7 @@
+---
+path: "/hello-world"
+date: 2020-08-24T17:12:33.962Z
+title: "My First Gatsby Post"
+---
+
+Oooooh-weeee, my first blog post!
